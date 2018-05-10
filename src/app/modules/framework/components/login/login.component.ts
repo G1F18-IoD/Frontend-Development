7adf9567f995d72725a545a2ab80fbf1be17ae73
Anders Thygesen <anthy@student.sdu.dk>
2018-05-10 14:45:35 +0200

    Register now works from /login

--- src/app/modules/framework/components/login/login.component.ts
+++ src/app/modules/framework/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
 import { GlobalService } from '../../services/global.service';
+import { HttpDefined } from '../../interfaces/http-defined';
+import { HttpReqsService } from '../../framework-export-barrel';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +18,7 @@ export class LoginComponent implements OnInit {
   public password;
 
   constructor(
+    private httpReqs: HttpReqsService,
     private authenticationService: AuthenticationService,
     private router: Router,
     private globalService: GlobalService
@@ -32,7 +35,15 @@ export class LoginComponent implements OnInit {
   }
 
   public register() {
-    this.router.navigate(['/register']);
+    let reqOption: HttpDefined = {
+      requestResource: 'http://skjoldtoft.dk/daniel/hab/index.php',
+      data: {class: "authentication",
+      method: "register", username: this.username, password: this.password },
+      statusCode: [200]
+    };
+    this.httpReqs.sendPostRequest(reqOption).subscribe((data) => {
+      this.login();
+    });
   }
 
 }
