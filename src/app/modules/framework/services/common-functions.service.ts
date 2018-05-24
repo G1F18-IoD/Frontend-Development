ab518518187fd22980653ddf1feae94cc6285323
Anders Thygesen <anthy@student.sdu.dk>
2018-05-24 11:19:56 +0200

    Fixed the scss files, so that they use the global variables.

--- src/app/modules/framework/services/common-functions.service.ts
+++ src/app/modules/framework/services/common-functions.service.ts
@@ -9,9 +9,6 @@ export class CommonFunctionsService {
   constructor() { }
 
   public shuffleArray(array, loopTimes = 10) {
-    /*if(loopTimes == 'undefined') {
-      loopTimes = 10;
-    }*/
     for(let x = 0; x < loopTimes; x++) {
       for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
